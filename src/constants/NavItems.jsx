3a18f75901bea401e991ba59c41282b75a223e79
Abby McInerney <44789458+abbymac@users.noreply.github.com>
2024-01-25 14:24:16 -0700

    include build (#4)

--- src/constants/NavItems.jsx
+++ src/constants/NavItems.jsx
@@ -1,22 +1,24 @@
 import EmailIcon from '@mui/icons-material/Email';
-import { LinkedIn, GitHub, Home } from '@mui/icons-material';
+import { LinkedIn, GitHub, Home, Article } from '@mui/icons-material';
 import theme from '../theme';
 import figma from '../assets/icons/figma.svg';
+import resume from '../assets/resume.pdf';
 
 const FigmaIcon = () => (
     <img src={figma} style={{ width: 24, height: 24, color: theme.palette.secondary.main }} alt="Figma" />
-)
+);
+
 
 export const NavItems = {
     Email: {
         icon: <EmailIcon sx={{ color: theme.palette.secondary.main, fontSize: 'inherit' }} />,
         text: 'Email',
-        link: 'mailto:test@test.com'
+        link: 'mailto:abbylangmcinerney@gmail.com'
     },
     LinkedIn: {
         icon: <LinkedIn sx={{ color: theme.palette.secondary.main, fontSize: 'inherit' }} />,
         text: 'LinkedIn',
-        link: 'https://www.linkedin.com'
+        link: 'https://www.linkedin.com/in/abby-mcinerney/'
     },
     GitHub: {
         icon: <GitHub sx={{ color: theme.palette.secondary.main, fontSize: 'inherit' }} />,
@@ -27,6 +29,11 @@ export const NavItems = {
         icon: <FigmaIcon />,
         text: 'Figma',
         link: 'https://www.figma.com/file/JHw3TO7Va4WhVeevXnrrji/Resume-Site?type=design&node-id=0%3A1&mode=design&t=9hgJcjzqaRLwQtBG-1'
+    },
+    Resume: {
+        icon: <Article sx={{ color: theme.palette.secondary.main, fontSize: 'inherit' }}/>,
+        text: 'Resume',
+        link: resume
     }
 };
 
